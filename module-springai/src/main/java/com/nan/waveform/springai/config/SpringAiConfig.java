1d37cf7b66f13b709804befcd8a6cf2605b29107
nanchao <2635469649@qq.com>
2026-06-22 12:43:25 +0800

    module-springai模块---QwenChat：通过MessageWindowChatMemory添加会话记忆功能；添加会话历史功能（初步方案：保存在前端localStorage中）。----20260622----nanmingjushi

--- module-springai/src/main/java/com/nan/waveform/springai/config/SpringAiConfig.java
+++ module-springai/src/main/java/com/nan/waveform/springai/config/SpringAiConfig.java
@@ -1,7 +1,11 @@
 package com.nan.waveform.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
@@ -12,20 +16,33 @@ import org.springframework.context.annotation.Primary;
  * @author nan chao
  * @since 2026/6/18 17:09
  *
- * Spring AI 多模型统一装配中心
+ * Spring AI 多模型统一装配中心（集成 会话日志，会话记忆）
  */
 
 @Configuration
 public class SpringAiConfig {
 
+
+    //会话记忆
+    @Bean
+    public ChatMemory chatMemory(){
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(new InMemoryChatMemoryRepository())
+                .maxMessages(20)  //保留最近20条会话记忆
+                .build();
+    }
+
     /**
      * 1. 专门装配本地 Ollama (DeepSeek) 的 ChatClient 实例
      */
     @Bean
-    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel,ChatMemory chatMemory) {
         return ChatClient.builder(ollamaChatModel)
                 .defaultSystem("你是一个电力系统专家，擅长解答关于试验录波文件快速解析的问题。")
-                .defaultAdvisors(new SimpleLoggerAdvisor())  // 加会话日志，普通对话和流式对话都会被打印
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(), // 加会话日志，普通对话和流式对话都会被打印
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()  //加会话记忆
+                )
                 .build();
     }
 
@@ -33,10 +50,13 @@ public class SpringAiConfig {
      * 2. 专门装配云端百炼 (Qwen3.7-Plus) 的 ChatClient 实例
      */
     @Bean
-    public ChatClient qwenChatClient(OpenAiChatModel openAiChatModel) {
+    public ChatClient qwenChatClient(OpenAiChatModel openAiChatModel,ChatMemory chatMemory) {
         return ChatClient.builder(openAiChatModel)
                 .defaultSystem("你是一个电力系统专家，擅长解答关于试验录波文件快速解析的问题。")
-                .defaultAdvisors(new SimpleLoggerAdvisor())  // 加会话日志，普通对话和流式对话都会被打印
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()  //加会话记忆
+                )  // 加会话日志，普通对话和流式对话都会被打印
                 .build();
     }
 
--- module-springai/src/main/java/com/nan/waveform/springai/controller/QwenChatController.java
+++ module-springai/src/main/java/com/nan/waveform/springai/controller/QwenChatController.java
@@ -1,12 +1,15 @@
 package com.nan.waveform.springai.controller;
+
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 /**
  * @author nan chao
@@ -17,6 +20,7 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequestMapping("/ai/qwen") //
 public class QwenChatController {
+
     private final ChatClient qwenChatClient;
 
     // 名字必须严格对应 Config 里的方法名 qwenChatClient
@@ -26,14 +30,32 @@ public class QwenChatController {
 
     //普通
     @GetMapping("/chat")
-    public String qwenChat(@RequestParam String prompt) {
-        return this.qwenChatClient.prompt().user(prompt).call().content();
+    public String qwenChat(@RequestParam String prompt,@RequestParam(required = false) String chatId) {
+
+        //如果 chatId 为空，自动归入默认全局会话空间，保证记忆切面正常运作
+        String finalChatId = StringUtils.hasText(chatId) ? chatId : "default_chat_session";
+
+        return this.qwenChatClient
+                .prompt()
+                .user(prompt)
+                .advisors(spec -> spec.param(CONVERSATION_ID,finalChatId))
+                .call()
+                .content();
     }
 
     //流式
     @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> qwenStream(@RequestParam String prompt) {
-        return this.qwenChatClient.prompt().user(prompt).stream().content();
+    public Flux<String> qwenStream(@RequestParam String prompt,@RequestParam(required = false) String chatId) {
+
+        //如果 chatId 为空，自动归入默认全局会话空间，保证记忆切面正常运作
+        String finalChatId = StringUtils.hasText(chatId) ? chatId : "default_chat_session";
+
+        return this.qwenChatClient
+                .prompt()
+                .user(prompt)
+                .advisors(spec -> spec.param(CONVERSATION_ID,finalChatId))
+                .stream()
+                .content();
     }
 }
 
